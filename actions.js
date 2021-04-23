feb034db343d8aa6637cd6a7af9ca5d36af32b9b
Kavinchakkaravarthi <32533793+Kavinchakkaravarthi@users.noreply.github.com>
2021-04-23 14:31:46 +0530

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,115 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+document.documentElement.style.setProperty('--rotSpeed', '10s');
+
+var x = 10;
+object.onmousemove = function(){
+
+    x -= 0.5;
+    //document.getElementById('speed').innerHTML = x-=0.1;
+
+    var sp = x + 's';
+    document.documentElement.style.setProperty('--rotSpeed', sp);
+    
+    if(x <= 1){
+        x = 1;
+    }
+    
+    console.log('speed', x);
+}
+ */
+/* 
+
+document.documentElement.style.setProperty('--rotSpeed', '10s');
+var x = 10;
+
+
+
+function decr(){
+    x--;
+}
+
+function incr(){
+    x++;
+}
+
+document.getElementById('rectangle').onmouseover = setTimeout(decr, 100);
+document.getElementById('rectangle').onmouseout = setTimeout(incr, 100);
+
+ */
+
+
+
+/* function mouseOver() {   
+
+    for(let i=0; i<100000; i++){
+        x -= 0.0001;
+        var sp = x + 's';
+        //document.documentElement.style.setProperty('--rotSpeed', sp);
+        /* if(x <= 1){
+            x = 1;
+        } 
+    }
+}
+
+x=10;
+ function mouseOut() {   
+
+    for(let i=0; i<100000; i++){
+        x += 0.0001;
+        var sp = x + 's';
+        //document.documentElement.style.setProperty('--rotSpeed', sp);
+            if(x <= 10){
+                x = 10;
+            }
+        }
+    }   
+ */
+
+    //document.getElementById('speed').innerHTML = x-=0.1;
+   
+ /*    
+  
+ */
+  //document.getElementById('rectangle').style.height = "100vw";
+
+/* function mouseOut() {
+  document.getElementById('rectangle').style.height = "10vw";
+} */
+
+
+
+
+
+
+
+
